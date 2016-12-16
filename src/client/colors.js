c046698b592b9c667835f7e5c5ac80896b875a01
Brian Zhou <bzhou4@gmail.com>
2016-12-16 12:49:53 -0800

    updated a .js file with possible color-schemes to use

---
+++ src/client/colors.js
@@ -0,0 +1,19 @@
+module.exports = [
+'#F49AC2',
+'#CB99C9',
+'#C23B22',
+'#FFD1DC',
+'#DEA5A4',
+'#AEC6CF',
+'#77DD77',
+'#CFCFC4',
+'#B39EB5',
+'#FFB347',
+'#B19CD9',
+'#FF6961',
+'#03C03C',
+'#FDFD96',
+'#836953', // brown
+'#779ECB',
+'#966FD6'
+]
