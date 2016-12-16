09d8400c2beee872759ecf09f748fa5baffc07e9
Brian Zhou <bzhou4@gmail.com>
2016-12-15 21:51:16 -0800

    added NODE_ENV rule to check for when wp will run

--- src/server/middleware.js
+++ src/server/middleware.js
@@ -4,21 +4,25 @@ var webpack = require('webpack');
 var path = require('path');
 var webpackConfig = require('../../webpack.config');
 
+// may need to wrap this into NODE_ENV
 var compiler = webpack(webpackConfig);
 
 module.exports = function(app, express) {
 
-	app.use(webpackDevMiddleware(compiler, {
-		hot: true,
-		publicPath: webpackConfig.output.publicPath,
-		stats: { colors: true }
-	}));
+	// this check will turn off HMR
+	if(process.env.NODE_ENV === 'production') {
+		app.use(webpackDevMiddleware(compiler, {
+			hot: true,
+			publicPath: webpackConfig.output.publicPath,
+			stats: { colors: true }
+		}));
 
-	app.use(webpackHotMiddleware(compiler, {
-		log: console.log,
-		path: '/__webpack_hmr',
-		heartbeat: 10 * 1000,
-	}));
+		app.use(webpackHotMiddleware(compiler, {
+			log: console.log,
+			path: '/__webpack_hmr',
+			heartbeat: 10 * 1000,
+		}));
+	}
 
 	app.use(function(req, res, next) {
 		console.log('[LOG @ Middleware] handling' + req.url);
